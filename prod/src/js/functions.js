895a045a640572bc3fc79376bb841b86d95e1984
Narayen <36738256+Narayen@users.noreply.github.com>
2020-03-25 16:40:33 +0530

    Update functions.js

--- prod/src/js/functions.js
+++ prod/src/js/functions.js
@@ -1,82 +1,82 @@
 function submit_form() {
 
-	var name = document.getElementById('user_name').value;
-	var email = document.getElementById('user_email').value;
-	var phone = document.getElementById('user_phone').value;
-	var message = document.getElementById('user_msg').value;
+	// var name = document.getElementById('user_name').value;
+	// var email = document.getElementById('user_email').value;
+	// var phone = document.getElementById('user_phone').value;
+	// var message = document.getElementById('user_msg').value;
 
-    // Validate if the text field is empty or data is proper
-    if($('#user_name').val().length>0) {
+    // // Validate if the text field is empty or data is proper
+    // if($('#user_name').val().length>0) {
         
-		//here it'll take the value without whitespaces only at the beginning and the end.
-		if($('#user_name').val().trim()=='') {
-            alert('Please enter your name');
-            $('#user_name').val('');
-            $('#user_name').focus();
-            return;
-        }
-    } else {
-        alert('Please enter your name');
-        $('#user_name').val('');
-        $('#user_name').focus();
-        return;
-    }
+	// 	//here it'll take the value without whitespaces only at the beginning and the end.
+	// 	if($('#user_name').val().trim()=='') {
+    //         alert('Please enter your name');
+    //         $('#user_name').val('');
+    //         $('#user_name').focus();
+    //         return;
+    //     }
+    // } else {
+    //     alert('Please enter your name');
+    //     $('#user_name').val('');
+    //     $('#user_name').focus();
+    //     return;
+    // }
 	
-	if($('#user_email').val().length>0) {
+	// if($('#user_email').val().length>0) {
         
-		//here it'll take the value without whitespaces only at the beginning and the end.
-		if($('#user_email').val().trim()=='') {
-            alert('Please enter the Email');
-            $('#user_email').val('');
-            $('#user_email').focus();
-            return;
-        }
-    } else {
-        alert('Please enter the Email');
-        $('#user_email').val('');
-        $('#user_email').focus();
-        return;
-	}
+	// 	//here it'll take the value without whitespaces only at the beginning and the end.
+	// 	if($('#user_email').val().trim()=='') {
+    //         alert('Please enter the Email');
+    //         $('#user_email').val('');
+    //         $('#user_email').focus();
+    //         return;
+    //     }
+    // } else {
+    //     alert('Please enter the Email');
+    //     $('#user_email').val('');
+    //     $('#user_email').focus();
+    //     return;
+	// }
 	
-	// Validate if the text field is empty or data is proper
-    if($('#user_phone').val().length>0) {
+	// // Validate if the text field is empty or data is proper
+    // if($('#user_phone').val().length>0) {
         
-		//here it'll take the value without whitespaces only at the beginning and the end.
-		if($('#user_phone').val().trim()=='') {
-            alert('Please enter the phone number');
-            $('#user_phone').val('');
-            $('#user_phone').focus();
-            return;
-        }
-    } else {
-        alert('Please enter the phone number');
-        $('#user_phone').val('');
-        $('#user_phone').focus();
-        return;
-	}
+	// 	//here it'll take the value without whitespaces only at the beginning and the end.
+	// 	if($('#user_phone').val().trim()=='') {
+    //         alert('Please enter the phone number');
+    //         $('#user_phone').val('');
+    //         $('#user_phone').focus();
+    //         return;
+    //     }
+    // } else {
+    //     alert('Please enter the phone number');
+    //     $('#user_phone').val('');
+    //     $('#user_phone').focus();
+    //     return;
+	// }
 
-    // Validate if the text field is empty or data is proper
-    if($('#user_msg').val().length>0) {
+    // // Validate if the text field is empty or data is proper
+    // if($('#user_msg').val().length>0) {
         
-		//here it'll take the value without whitespaces only at the beginning and the end.
-		if($('#user_msg').val().trim()=='') {
-            alert('Please enter your message');
-            $('#user_msg').val('');
-            $('#user_msg').focus();
-            return;
-        }
-    } else {
-        alert('Please enter your message');
-        $('#user_msg').val('');
-        $('#user_msg').focus();
-        return;
-	}
+	// 	//here it'll take the value without whitespaces only at the beginning and the end.
+	// 	if($('#user_msg').val().trim()=='') {
+    //         alert('Please enter your message');
+    //         $('#user_msg').val('');
+    //         $('#user_msg').focus();
+    //         return;
+    //     }
+    // } else {
+    //     alert('Please enter your message');
+    //     $('#user_msg').val('');
+    //     $('#user_msg').focus();
+    //     return;
+	// }
 	
 	alert('OK');
-	alert(name);
-	alert(email);
+	alert(NAME);
+	alert(EMAIL);
 	alert(phone);
-	alert(message);
+	alert(COMMENTS);
 	
 	// $.post("/submit", {
 	// 	user_name: name,
